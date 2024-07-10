89ed6c2fd1d3a6b07b5179206d7d812bf5c25907
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2024-07-10 22:21:50 +0800

    :: worked on: updated routes

--- src/App.js
+++ src/App.js
@@ -2,38 +2,17 @@
 import React from "react";
 import "./App.css";
 
-// components
-import NavBar from "./components/navbar/NavBar";
-import Header from "./components/header/Header";
-import Subheader from "./components/subheader/Subheader";
-import Categories from "./components/categories/Categories";
-import ItemList from "./components/itemlisting/ItemList";
-import Banner from "./components/banner/Banner";
-import BorrowingPost from "./components/borrowingposts/BorrowingPost";
-import FAB from "./components/fab/FAB";
-import Footer from "./components/footer/Footer";
+// pages, public
+import Home from "./pages/public/Home.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const handleFabClick = (action) => {
-    if (action === "add-item") {
-      console.log("Add Item button clicked");
-    } else if (action === "create-post") {
-      console.log("Create Post button clicked");
-    }
-  };
-
   return (
-    <div>
-      <NavBar />
-      <Header />
-      <Subheader />
-      <Categories />
-      <ItemList />
-      <Banner />
-      <BorrowingPost />
-      <Footer />
-      <FAB icon="+" onClick={handleFabClick} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/public/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+// components
+import NavBar from "../../components/navbar/NavBar";
+import Header from "../../components/header/Header";
+import Subheader from "../../components/subheader/Subheader";
+import Categories from "../../components/categories/Categories";
+import ItemList from "../../components/itemlisting/ItemList";
+import Banner from "../../components/banner/Banner";
+import BorrowingPost from "../../components/borrowingposts/BorrowingPost";
+import FAB from "../../components/fab/FAB";
+import Footer from "../../components/footer/Footer";
+
+function Home() {
+  const handleFabClick = (action) => {
+    if (action === "add-item") {
+      console.log("Add Item button clicked");
+    } else if (action === "create-post") {
+      console.log("Create Post button clicked");
+    }
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <Subheader />
+      <Categories />
+      <ItemList />
+      <Banner />
+      <BorrowingPost />
+      <Footer />
+      <FAB icon="+" onClick={handleFabClick} />
+    </div>
+  );
+}
+
+export default Home;
