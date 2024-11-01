d5c7635c7abae823bb060e3ecbeb5b6f7e2d00f9
Kurisu220 <146604139+Kurisu220@users.noreply.github.com>
2024-11-01 17:51:36 +0800

    update sa protect route

--- src/components/Protected Route/ProtectedRoute.js
+++ src/components/Protected Route/ProtectedRoute.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
-const roleHierarchy = { student: 1, admin: 2, superadmin: 3 }; // Hierarchical roles
-
-const ProtectedRoute = ({ requiredRole }) => {
+const ProtectedRoute = ({ allowedRoles }) => {
   const { user } = useAuth();
 
-  if (!user || roleHierarchy[user.role] < roleHierarchy[requiredRole]) {
+  // Check if the user is logged in and has an allowed role
+  const hasAccess = user && allowedRoles.includes(user.role);
+
+  // If not logged in or no access, redirect to admin login
+  if (!hasAccess) {
     return <Navigate to="/admin-login" />;
   }
 
+  // Render the outlet (the child routes)
   return <Outlet />;
 };
 
