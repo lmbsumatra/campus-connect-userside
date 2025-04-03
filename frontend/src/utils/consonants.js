9f425a28db8169ec9e10f40be2ad83c848b6aba6
lmbsumatra <122442812+lmbsumatra@users.noreply.github.com>
2025-04-03 08:59:05 +0800

    :: build me up, buttercup dont break my break

--- frontend/src/utils/consonants.js
+++ frontend/src/utils/consonants.js
@@ -38,9 +38,9 @@ export const defaultImages = [
   `https://res.cloudinary.com/campusconnectcl/image/upload/v1735920030/cc/upjauojvyedsriwhdcks.svg`,
 ];
 
-export const REACT_APP_API_URL = "http://localhost:3001";
-export const baseApi = REACT_APP_API_URL || "http://localhost:3001";
-export const baseUrl = "http://localhost:3000";
+export const REACT_APP_API_URL = "https://api.rentupeers.shop";
+export const baseApi = REACT_APP_API_URL || "https://api.rentupeers.shop";
+export const baseUrl = "https://rentupeers.shop";
 export const REACT_APP_GOOGLE_CLIENT_ID =
   "474440031362-3ja3qh8j5bpn0bfs1t7216u8unf0ogat.apps.googleusercontent.com";
 
