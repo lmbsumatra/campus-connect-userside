a7f2716c9a1577b4e4664c9f94e336be81372611
Kurisu220 <manzanojc02@gmail.com>
2025-05-04 12:19:17 +0800

    DB UPDATE BELOW and minor changes
    ALTER TABLE auditlogs MODIFY COLUMN admin_id INT NULL;

--- backend/models/AuditLogModel.js
+++ backend/models/AuditLogModel.js
@@ -11,7 +11,7 @@ const AuditLog = sequelize.define(
     },
     admin_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
     role: {
       type: DataTypes.STRING,
